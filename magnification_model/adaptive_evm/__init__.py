01898d13dccf4fb1aa4b2803018eb38664d58e4c
yukun zhang <66954391+cxhwxhn@users.noreply.github.com>
2026-06-22 20:07:41 +0800

    Delete magnification_model directory

--- magnification_model/adaptive_evm/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Adaptive dynamic weak-texture video amplification."""
-
-from .amplification import adaptive_dynamic_weak_texture_amplification
-from .config import Params
-
-__all__ = ["Params", "adaptive_dynamic_weak_texture_amplification"]
